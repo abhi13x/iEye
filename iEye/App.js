ac2dfe4427beb239617f54f999bc6ff02a6f0e2d
abhi13x <abhishekbhatnagar13x@gmail.com>
2022-10-30 19:31:55 +0530

    added background color

--- iEye/App.js
+++ iEye/App.js
@@ -1,20 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={styles.top}>
+    <View style={styles.middle1}>
+    <View style={styles.middle2}>
+    <View style={styles.bottom}>
       <StatusBar style="auto" />
+      </View>
+      </View>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  top: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#142850',
     alignItems: 'center',
     justifyContent: 'center',
+    width: "100%",
+    height: 30
   },
+  middle1: {
+    flex: 0.5,
+    backgroundColor: '#27496D',
+    width: "100%",
+    height: 30
+  },
+  
+  middle2: {
+    flex: 0.5,
+    backgroundColor: '#0C7B93',
+    width: "100%",
+    height: 30
+  },
+  bottom: {
+    flex: 0.5,
+    backgroundColor: '#00A8CC',
+    width: "100%",
+    height: 30
+  }
 });
